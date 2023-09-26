570b75d1d6d79252fc2d933c74a5bb43110dcd8b
sbmrv <shubhamrv19@gmail.com>
2023-09-27 00:01:09 +0530

    auth0 update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,13 +7,13 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-     domain="dev-dnb8upwxx8mztegu.us.auth0.com"
-     clientId="49lxutbXQ84OcBggLU0xUOl8BeU24K1q"
-     authorizationParams={{
-      redirect_uri: "http://localhost:5173/"
-     }}
-     audience="http://localhost:8000"
-     scope="openid profile email"
+      domain="dev-dnb8upwxx8mztegu.us.auth0.com"
+      clientId="49lxutbXQ84OcBggLU0xUOl8BeU24K1q"
+      authorizationParams={{
+        redirect_uri: "https://realtor-github-io-three.vercel.app/",
+      }}
+      audience="http://localhost:8000"
+      scope="openid profile email"
     >
       <App />
     </Auth0Provider>
