36f741cbbf439ae6c5efadef21c12f0886bc4e0c
sbmrv <shubhamrv19@gmail.com>
2023-09-26 19:38:41 +0530

    first commit

---
+++ server/routes/userRoute.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+  bookVisit,
+  cancelBooking,
+  createUser,
+  getAllBookings,
+  toFav,
+  getAllFavorites,
+} from "../controllers/userCntrl.js";
+import jwtCheck from "../config/auth0Config.js";
+
+
+const router = express.Router();
+
+// router.post("/register" , createUser);
+router.post("/bookVisit/:id", bookVisit);
+router.post("/allBookings", getAllBookings);
+router.post("/removeBooking/:id", cancelBooking);
+router.post("/toFav/:rid", toFav);
+router.post("/allFav/", getAllFavorites);
+router.post("/register", jwtCheck, createUser);
+// router.post("/bookVisit/:id", jwtCheck, bookVisit);
+// router.post("/allBookings", getAllBookings);
+// router.post("/removeBooking/:id", jwtCheck, cancelBooking);
+// router.post("/toFav/:rid", jwtCheck, toFav);
+// router.post("/allFav/", jwtCheck, getAllFavorites);
+export { router as userRoute };
